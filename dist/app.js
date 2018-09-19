5144c8d126edaf8b30b4eccf78305ba5c84e4376
Joseph Alba <jalba19@gmail.com>
2018-09-19 11:56:38 -0400

    first commit

---
+++ dist/app.js
@@ -0,0 +1,9 @@
+function openSlideMenu() {
+    document.getElementById('side-menu').style.width = '250px';
+    document.getElementById('menu').style.marginLeft = '250px';
+}
+
+function closeSlideMenu() {
+    document.getElementById('side-menu').style.width = '0';
+    document.getElementById('menu').style.marginLeft = '0';
+}
