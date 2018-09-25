7a16b2cd3b15f7194b07de053481b5bead0f47dd
Joseph Alba <jalba19@gmail.com>
2018-09-25 10:10:29 -0400

    started email modal

--- dist/app.js
+++ dist/app.js
@@ -7,3 +7,7 @@ function closeSlideMenu() {
     document.getElementById('side-menu').style.width = '0';
     document.getElementById('menu').style.marginLeft = '0';
 }
+
+
+
+// const chg = document.querySelector('.button-1').style.color = 'yellow';
